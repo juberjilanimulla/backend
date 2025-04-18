a6687f48e52e6bd2f93bfa0798db9d89bc069c8b
Juber <juber.mulla@suyotechsolutions.com>
2025-04-18 16:56:35 +0530

    code structure

---
+++ src/models/postmodel.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    title: String,
+    content: String,
+    author: String,
+  },
+  { timestamps: true, versionKey: false }
+);
+
+function currentLocalTimePlusOffset() {
+  const now = new Date();
+  const offset = 5.5 * 60 * 60 * 1000;
+  return new Date(now.getTime() + offset);
+}
+
+postSchema.pre("save", function (next) {
+  const currentTime = currentLocalTimePlusOffset();
+  this.createdAt = currentTime;
+  this.updatedAt = currentTime;
+  next();
+});
+
+postSchema.pre("findOneAndUpdate", function (next) {
+  const currentTime = currentLocalTimePlusOffset();
+  this.set({ updatedAt: currentTime });
+  next();
+});
+
+const postmodel = model("post", postSchema);
+export default postmodel;
